8dc5b01f38fa56aad337f80759e0ee3ca2335cee
Quinton Savoie <Quinton Savoie@172.28.0.92>
2017-11-28 14:15:39 -0400

    Retrieve the id parameter

--- templates/article.php
+++ templates/article.php
@@ -1,3 +1,10 @@
 
 <div class="container">
         <h1 class="mt-4 mb-3">Articles</h1>
+        
+        <?php
+        var_dump($_GET);
+        //Retrieve the id parameter from the URL
+        $id = $_GET['id'];
+      ?>
+</div>
